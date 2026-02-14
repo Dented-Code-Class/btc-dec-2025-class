8b7fde55fd4850b8ae67712dfa4a045d51a501c0
Ashon Shakya <shakya.ashon@gmail.com>
2026-02-15 09:24:47 +1100

    update: user contact project update

---
+++ user-contacts/backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+
+configDotenv();
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/ntdl-d";
+
+app.use(express.json());
+app.use(cors());
+
+// GET base url
+app.get("/", (req, res) => {
+  return res.send({
+    status: "success",
+    message: "User API",
+  });
+});
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("MONGO CONNECTED", MONGO_URL);
+    app.listen(PORT, (error) => {
+      if (error) {
+        console.log(error);
+        console.log("SERVER did not start!");
+      } else {
+        console.log("Server started at PORT: ", PORT);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("ERROR MONGO");
+  });
